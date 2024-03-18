acce123f1db1c84675b4b6fe812d37ac485ff5f4
ShamimulArefin <shamimularefin100@gmail.com>
2024-03-19 03:30:08 +0800

    dskfkl

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-
+from nltk.corpus import stopwords
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 # Load data for car recommendation app
 @st.cache
 def load_data():
